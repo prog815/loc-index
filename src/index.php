78601ea9beab7287c82d8ed36dd5492c404e3964
prog <evprog@gmail.com>
2022-05-10 18:52:18 +0500

    выводит список файлов в интерфейсе

--- src/index.php
+++ src/index.php
@@ -11,6 +11,27 @@
 
 echo "привет всем" ;
 
+$link = mysqli_connect('mysql', 'user', 'mypassword','loc-poisk') or die('Не удалось соединиться: ' . mysqli_error()) ;
+
+// mysql_select_db('loc-poisk') or die('Не удалось выбрать базу данных') ;
+
+$query = 'SELECT * FROM files';
+$result = mysqli_query($link,$query) or die('Запрос не удался: ' . mysqli_error()) ;
+
+echo "<table>\n";
+while ($line = mysqli_fetch_array($result)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+
+mysqli_free_result($result);
+
+mysqli_close($link);
+
     ?>
 
 
